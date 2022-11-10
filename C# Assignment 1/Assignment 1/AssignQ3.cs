0a08f5250da47e87b22fdaa9b321ee1a526fb429
Nithya <115216293+nithyasrikasidu@users.noreply.github.com>
2022-11-10 16:40:29 +0000

    Add files via upload

---
+++ C# Assignment 1/Assignment 1/AssignQ3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class AssignQ3
+    {
+        public static void Main()
+        {
+            int[] a = new int[100];
+            int i, n, sum = 0;
+
+            Console.Write("Enter array limit :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Enter the numbers as per ur limit {0} : ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+
+            Console.Write("Sum of all elements stored in the array is : {0}\n\n", sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Assignment 1/Assignment 1/AssignQ6.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+   public class AssignQ6
+    {
+        public static void Main()
+        {
+            Console.WriteLine("--------SELECT OPTION----------");
+
+
+            
+            Console.WriteLine("Press 1 for Magazine");
+            Console.WriteLine("Press 2 for Novel");
+            Console.WriteLine("Press 3 for ReferenceBook");
+            Console.WriteLine("Press 4 for Miscellaneous \n");
+            Console.WriteLine("Enter ur option: ");
+
+
+            int action = Convert.ToInt32(Console.ReadLine());
+
+            switch (action)
+            {
+                case 1:
+
+                    {
+
+                        Console.WriteLine("Book_Id:1");
+                        Console.WriteLine("Title: Hindu");
+                        Console.WriteLine("Price: 400");
+                        Console.WriteLine("Book Type: Magazine");
+
+
+                        break;
+                    }
+                case 2:
+                    {
+  
+                        Console.WriteLine("Book_Id:2");
+                        Console.WriteLine("Title:True friend");
+                        Console.WriteLine("Price: 250");
+                        Console.WriteLine("Book Type: Novel");
+                        break;
+                    }
+                case 3:
+                    {
+                  
+                        Console.WriteLine("Book_Id:3");
+                        Console.WriteLine("Title:ramayanam");
+                        Console.WriteLine("Price: 950");
+                        Console.WriteLine("Book Type: ReferenceBook");
+                        break;
+                    }
+                case 4:
+                    {
+                      
+                        Console.WriteLine("Book_Id:4");
+                        Console.WriteLine("Title: crack ");
+                        Console.WriteLine("Price: 400");
+                        Console.WriteLine("Book Type: Miscellaneous");
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Incorrect option");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
