a6f5361be4e39df3cf35663f7521975bb19b57a9
Nithya <115216293+nithyasrikasidu@users.noreply.github.com>
2022-12-13 11:21:16 +0000

    Add files via upload

---
+++ Hands on lab/HOL3/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hands_on_lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            Models.Product p = new Models.Product();
+
+            //pass a list of products to the view
+            return View(p.GetProducts());
+
+        }
+    }
+}
