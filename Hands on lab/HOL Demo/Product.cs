a84ad73133f6676e67aad619358ada7fd63f7ff0
Nithya <115216293+nithyasrikasidu@users.noreply.github.com>
2022-12-13 14:17:33 +0000

    Add files via upload

---
+++ Hands on lab/HOL Demo/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HOL_Demo.Models
+{
+    public class Product
+    {
+       
+public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public IEnumerable<Product> GetProducts()
+
+        {
+
+            return new List<Product>()
+
+ {
+
+                  new Product{ProductId=1,
+
+                 ProductName="Milk",Price=600},
+                  
+                  new Product{ProductId=2,
+
+                  ProductName="bag",Price=500},
+
+                  new Product{ProductId=3,
+
+                  ProductName="Mobile",Price=15000},
+
+ 
+
+ };
+
+        }
+
+
+    }
+}
